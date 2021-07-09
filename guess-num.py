32de09f74f6d2af680b4fd44c1e9471134614adf
Amywei <amywei2008@gmail.com>
2021-07-09 09:11:06 +0800

    Add new count

--- guess-num.py
+++ guess-num.py
@@ -1,14 +1,18 @@
 import random
 r = random.randint(1,100)
+guess_time = 1
 while True:
 	guess = input('請猜一個整數:')
 	guess = int(guess)
 	if guess == r:
 		print('你猜對了，真棒!')
+		print('你總共猜',guess_time,'次')
 		break
 	elif guess > r:
 		print('太大了!')
+		guess_time = guess_time +1
 	else:
 		print('太小了!')
+		guess_time = guess_time +1
 
 	
